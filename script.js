88c72d11952506335ef97b8a19ef70f56594ec66
Abdubosit01 <abdubosit0115@gmail.com>
2025-07-25 15:01:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+let data = ['olma', 'anor', 'kiwi', 'uzum']
+
+alert(data)
+
+
+let b = confirm('arrayga chiqsimi')
+
+if (b == true){
+    data.push('banan')
+    console.log(data);
+    
+}else{
+    data.pop()
+    console.log(data);
+    
+}
+
+
